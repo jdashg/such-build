24c84b31bac80f322712ecc6bcdbf6a8c6073d81
jdashg <jdashg+github@gmail.com>
2014-03-28 19:41:22 -0700

    No slow-scripts by default, and quiet GDB.

--- run-gdb-debug.sh
+++ run-gdb-debug.sh
@@ -6,5 +6,6 @@ cd $OBJDIR_RELPATH
 cd obj-dbg
 mkdir foo 2>/dev/null
 
+export JS_DISABLE_SLOW_SCRIPT_SIGNALS=1
 export MOZ_QUIET=1
-gdb --args ./dist/bin/firefox -profile foo -no-remote
+gdb -quiet --args ./dist/bin/firefox -profile foo -no-remote
--- run-opt-apitrace.sh
+++ run-opt-apitrace.sh
@@ -6,5 +6,6 @@ cd $OBJDIR_RELPATH
 cd obj-opt
 mkdir foo 2>/dev/null
 
+export JS_DISABLE_SLOW_SCRIPT_SIGNALS=1
 export MOZ_QUIET=1
 ~/bin/apitrace/build/apitrace trace ./dist/bin/firefox -profile foo -no-remote
