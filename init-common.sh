12d553ca0ef3421806c53c9254f6c492b2aaac28
jdashg <jdashg@gmail.com>
2016-04-05 15:38:54 -0700

    Simplify init-common and add shortcuts for utils.

--- init-common.sh
+++ init-common.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 such_src_dir=$PWD
-such_dir=$PWD/`dirname $0`
+such_dir=$such_src_dir/`dirname $0`
 
-rm $such_src_dir/../.such-probe 2>/dev/null
+rm ../.such-probe 2>/dev/null
 rm $such_dir/../.such-probe 2>/dev/null
 
-touch $such_src_dir/../.such-probe
+touch ../.such-probe
 if [ ! -e $such_dir/../.such-probe ]
 then
+  rm ../.such-probe
   echo "Source and Such dirs must share a parent dir."
   exit 1
 fi
-rm $such_src_dir/../.such-probe
+rm ../.such-probe
 
 echo "   much config"
 echo "               very default"
 echo "         wow"
 
-cp -fp $such_dir/defaults/.such-dirs.sh $such_dir/../.such-dirs.sh
+cp -fp $such_dir/defaults/.such-dirs.sh ../.such-dirs.sh
 
-echo "" >> $such_dir/../.such-dirs.sh
-echo "# Vars below are from init-with-cwd.sh." >> $such_dir/../.such-dirs.sh
-echo "such_dir=$such_dir" >> $such_dir/../.such-dirs.sh
-echo "such_src_dir=$such_src_dir" >> $such_dir/../.such-dirs.sh
+echo "" >> ../.such-dirs.sh
+echo "# Vars below are from init-with-cwd.sh." >> ../.such-dirs.sh
+echo "such_dir=$such_dir" >> ../.such-dirs.sh
+echo "such_src_dir=$such_src_dir" >> ../.such-dirs.sh
+
+echo "../such/mast.sh" > ../mast
+echo "../such/git-export.sh" > ../git-export
+echo "../such/hg-import.sh" > ../hg-import
+echo "../such/hg-unimport.sh" > ../hg-unimport
