885bb4834e1c888857425b4b3bc5632d0bc9507c
jdashg <jdashg@gmail.com>
2016-07-21 21:06:10 -0700

    Disable tests/accessibility and enable MOZ_GL_SPEW.

--- run-debug.sh
+++ run-debug.sh
@@ -9,5 +9,6 @@ mkdir foo 2>/dev/null
 export JS_DISABLE_SLOW_SCRIPT_SIGNALS=1
 export MOZ_QUIET=1
 export MOZ_SEPARATE_CHILD_PROCESS=1
+export MOZ_GL_SPEW=1
 #export XPCOM_DEBUG_BREAK=trap
 ./dist/bin/firefox -console -profile foo -no-remote
