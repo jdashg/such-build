16b948f445aaa86a88d3f0721e65c1670566884e
Jeff Gilbert <jdashg@gmail.com>
2017-03-28 19:22:47 -0700

    Fix init-common for Windows.

--- init-common.sh
+++ init-common.sh
@@ -11,19 +11,33 @@ echo '   much config'
 echo '               very default'
 echo '         wow'
 
-such_dir="$src_dir"/`dirname $0`
-such_dir=`realpath "$such_dir"`
+objs_rel_dir=../objs
 
-objs_dir=`realpath "$src_dir"/../objs`
+####
+
+such_dir="$src_dir"/`dirname $0`
+objs_dir="$src_dir"/"$objs_rel_dir"
+
+# `realpath` isn't present on moz-build.
+pushd "$such_dir" >/dev/null
+such_dir=`pwd`
+popd >/dev/null
+
+pushd "$objs_dir" >/dev/null
+objs_dir=`pwd`
+popd >/dev/null
 
 echo '#! /bin/bash'                >  "$src_dir"/../.such-dirs.sh
 echo '# From such/init-common.sh:' >> "$src_dir"/../.such-dirs.sh
 echo 'such_dir='"$such_dir"        >> "$src_dir"/../.such-dirs.sh
 echo 'objs_dir='"$objs_dir"        >> "$src_dir"/../.such-dirs.sh
 
-obj_dir="$objs_dir"/`basename "$src_dir"`
+####
 
-cat "$such_dir"/defaults/.mozconfig         >  "$src_dir"/.mozconfig
-echo ''                                     >> "$src_dir"/.mozconfig
-echo '# From such/init-common.sh:'          >> "$src_dir"/.mozconfig
-echo 'mk_add_options MOZ_OBJDIR='"$obj_dir" >> "$src_dir"/.mozconfig
+obj_dir_name=`basename "$src_dir"`
+moz_objdir="@TOPSRCDIR@/$objs_rel_dir/$obj_dir_name"
+
+cat "$such_dir"/defaults/.mozconfig            >  "$src_dir"/.mozconfig
+echo ''                                        >> "$src_dir"/.mozconfig
+echo '# From such/init-common.sh:'             >> "$src_dir"/.mozconfig
+echo 'mk_add_options MOZ_OBJDIR='"$moz_objdir" >> "$src_dir"/.mozconfig
