9503e522f8801f3cacfe765ed93211dbe46fe3f7
Jeff Gilbert <jdashg@gmail.com>
2017-05-08 19:52:43 -0700

    Update run-lldb-debug for $objs_dir

--- run-lldb-debug.sh
+++ run-lldb-debug.sh
@@ -2,10 +2,9 @@
 . `dirname $0`/../.such-dirs.sh
 
 src_dir_name=`basename $PWD`
-obj_dir_name=$src_dir_name'-obj'
 
-cd $such_obj_dir
-cd $obj_dir_name
+cd "$objs_dir"
+cd "$src_dir_name"
 mkdir foo 2>/dev/null
 
 export MOZ_QUIET=1
